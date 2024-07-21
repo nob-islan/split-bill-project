1f000b54cb9b098beada3dc6a3af69792cfa900b
nob-islan <nob.snail@gmail.com>
2024-07-21 07:36:04 +0000

    リクエストの形式のみ確定

--- split-bill-web/src/action/PriceCalcAction.ts
+++ split-bill-web/src/action/PriceCalcAction.ts
@@ -7,7 +7,19 @@ export const callCalc = () => {
   return async (dispatch: Dispatch) => {
     const res = await axios.post(
       `http://localhost:8080/api/v1/price-calculation/calc`,
-      {} // TODO リクエスト
+      {
+        memberInputInfoDtoList: [
+          {
+            memberName: 'nob',
+            memberRole: '部長'
+          },
+          {
+            memberName: 'nob2',
+            memberRole: '課長代行'
+          }
+        ],
+        totalPrice: 13000
+      } // TODO リクエスト
     );
     console.log(res);
   };
